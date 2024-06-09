c21e28532af6fb713ef58c3286f8afff43e58fef
Чулисов Егор Владимирович <yegorchulisov@mail.ru>
2024-06-10 02:08:12 +0300

    changed port

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 10000;
 const path = require('path')
 
 app.use(express.static(__dirname + '/dist/ognd'))
