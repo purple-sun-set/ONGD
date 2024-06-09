083968638d099aa4d49b08d6db9c1f84b34bc953
Чулисов Егор Владимирович <yegorchulisov@mail.ru>
2024-06-10 02:03:51 +0300

    changed server path

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const app = express()
 const port = process.env.PORT || 4000;
 const path = require('path')
 
-app.use(express.static(__dirname + '/dist/OGND'))
+app.use(express.static(__dirname + '/dist/ognd'))
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname + '/dist/OGND'))
+  res.sendFile(path.join(__dirname + '/dist/ognd'))
 })
 
 app.listen(port, () => {
